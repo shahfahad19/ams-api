da7040261bc08be5d44e0ff511402f7c58547ae9
shahfahad19 <sfahad4280@gmail.com>
2023-01-12 17:12:11 +0500

    ok

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
 
 const AppError = require('./utils/appError');
@@ -7,6 +8,18 @@ const globalErrorHandler = require('./controllers/errorController');
 const adminRouter = require('./routes/adminRoutes');
 const studentRouter = require('./routes/studentRoutes');
 const teacherRouter = require('./routes/teacherRoutes');
+
+dotenv.config({ path: './config.env' });
+
+process.on('uncaughtException', (err) => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err);
+    process.exit(1);
+});
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.set('strictQuery', false);
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 app.use(express.json());
 app.get('/', (req, res) => {
@@ -23,4 +36,15 @@ app.all('*', (req, res, next) => {
 
 app.use(globalErrorHandler);
 
-module.exports = app;
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+    console.log(err.stack);
+    server.close(() => {
+        process.exit(1);
+    });
+});
